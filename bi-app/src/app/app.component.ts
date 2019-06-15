3421fc18dc98511608921abfb07331fcfc436c39
Mohammad Anouti <manouti1988@gmail.com>
2019-06-15 13:20:20 +0300

    add table listing
    Signed-off-by: Mohammad Anouti <manouti1988@gmail.com>

--- bi-app/src/app/app.component.ts
+++ bi-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'KI Financial Indicator';
+  title = "KI Financial Indicator";
+  KPIs = [{ name: "KPI1" }, { name: "KPI2" }, { name: "KPI3" }];
 }
