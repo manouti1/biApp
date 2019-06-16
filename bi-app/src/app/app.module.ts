8802897a2db68b55c9f2beea1975d3247c660dcb
Mohammad Anouti <manouti1988@gmail.com>
2019-06-16 19:50:57 +0300

    add reactive form
    Signed-off-by: Mohammad Anouti <manouti1988@gmail.com>

--- bi-app/src/app/app.module.ts
+++ bi-app/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+
 import { HttpClientModule } from '@angular/common/http';
 import { HighchartsChartModule } from 'highcharts-angular';
 
@@ -24,6 +25,7 @@ import { CreateKpiComponent } from './create-kpi/create-kpi.component';
   imports: [
     BrowserModule,
     HighchartsChartModule ,
+    ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule
