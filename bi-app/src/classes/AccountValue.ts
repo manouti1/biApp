77c43a295fbadb74227faebe9f06723d0dd7d6a4
Mohammad Anouti <manouti1988@gmail.com>
2019-06-16 18:51:55 +0300

    add models
    Signed-off-by: Mohammad Anouti <manouti1988@gmail.com>

---
+++ bi-app/src/classes/AccountValue.ts
@@ -0,0 +1,7 @@
+import { Account } from "./Account";
+export class AccountValue extends Account {
+  public accountValueId: number;
+  public accountId: number;
+  public amount: number;
+  public year: number;
+}
