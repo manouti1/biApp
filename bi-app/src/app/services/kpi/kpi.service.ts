0c372ab0700d9f8ac69e56e2f1deae9dfec4a406
Mohammad Anouti <manouti1988@gmail.com>
2019-06-20 08:26:54 +0300

    add kpi service

---
+++ bi-app/src/app/services/kpi/kpi.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {KPI} from '../../../classes/KPI';
+import {HttpService} from '../http.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class KpiService  extends HttpService<KPI, number> {
+
+  constructor(protected _http: HttpClient) {
+    super(_http, 'http://localhost:8080/api/kpi');
+   }
+}
